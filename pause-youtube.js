a93c60d346f72c641e17b3746cfc816f25d57faf
Anees Ahee <aneesahee@gmail.com>
2015-08-04 00:09:57 +0100

    Works with new YouTube player

--- pause-youtube.js
+++ pause-youtube.js
@@ -1,6 +1,6 @@
 (function() {
 	console.log("Trying to pause video...");
-	var pauseButton = document.getElementsByClassName("ytp-button ytp-button-pause")[0];
+	var pauseButton = document.getElementsByClassName("ytp-play-button ytp-button")[0];
 	if (typeof pauseButton != "undefined") {
 		pauseButton.click();
 		console.log("Paused YouTube video.");
